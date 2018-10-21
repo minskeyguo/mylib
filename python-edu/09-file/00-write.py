c58e4977788fccefb22e7a81269ba679931ac10a
minskeyguo <minskey.guo@outlook.com>
2018-10-21 21:06:33 +0800

    add examples file operation

---
+++ python-edu/09-file/00-write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+f = open("f.txt", "w")
+
+f.write("This is my first file\n")
+
+f.close()
+
+
+
+# when write failed, f.close() won't be called
+# so:
+#   with open("f.txt", "w") as f
