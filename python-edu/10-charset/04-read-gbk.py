f517b713da952a0cfededb15ca4e712bbcccf244
minskeyguo <minskey.guo@outlook.com>
2018-10-26 23:55:24 +0800

    add word frequency examples

---
+++ python-edu/10-charset/04-read-gbk.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import codecs
+import sys
+
+if len(sys.argv) > 1:
+    FILE=sys.argv[1]
+else:
+    FILE="qing-tong-kui-hua-gbk.txt"
+
+fp = codecs.open(FILE, 'r', 'gb18030')
+texts = fp.read()
+fp.close()
+print(texts)
+
+
+
+
+
--- python-edu/10-charset/09-work-freq.py
+++ python-edu/10-charset/09-work-freq.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
+import sys
+
+if len(sys.argv) > 1:
+    FILE=sys.argv[1]
+else:
+    FILE="qing-tong-kui-hua.txt"
+
 # unicode: 基本汉字 0x4e00, 0x9fa5
-
 scope = ((0x4e00,0x9fa5),)
-
-FILE="qing-tong-kui-hua.txt"
 
 fp = open(FILE, 'r')
 texts = fp.read()
@@ -17,6 +21,8 @@ li = []
 for ch in texts:
     li.append(ch)
 
+words = 0
+punc = 0
 wfreq = {}
 pfreq = {}
 for w in li:
@@ -25,19 +31,21 @@ for w in li:
             wfreq[w] += 1
         else:
             wfreq[w] = 1
+        words += 1
     else:
         if w in pfreq:
             pfreq[w] += 1
         else:
             pfreq[w] = 1
+        punc += 1
 
 wfreq = sorted(wfreq.items(), key=lambda x:x[1], reverse=True)
 pfreq = sorted(pfreq.items(), key=lambda x:x[1], reverse=True)
 
-for entry in wfreq[:100]:
-    print("%s\t: %d" %entry)
+for entry in wfreq:
+    print("%s\t: %d  %f" %(entry[0], entry[1], entry[1]/words))
 
-for entry in pfreq[:100]:
+for entry in pfreq:
     print("%s\t: %d" %entry)
 
 
